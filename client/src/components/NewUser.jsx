7680d2fc67fe859fc56847427a30bffa61fe770e
nvmarzakov <nikolay.marzakov@gmail.com>
2023-12-03 11:48:10 +0200

    add user button
    createUserModal and button=> NewUser

--- client/src/components/NewUser.jsx
+++ client/src/components/NewUser.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react"
+import CreateUserModal from "./CreateUserModal"
+
 export default function NewUser() {
+    const [showCreateModal, setShowCreateModal] = useState(false)
+
+
+    const createUserClickHandler = () => {
+        setShowCreateModal(true);
+    }
+
+    const hideCreateUserModal = () => {
+        setShowCreateModal(false)
+    }
+
     return (
-        <button className="btn-add btn">Add new user</button> 
+        <div>
+            <button className="btn-add btn" onClick={createUserClickHandler}>Add new user</button>
+
+            {showCreateModal && <CreateUserModal hideModal={hideCreateUserModal} />}
+        </div>
     )
 }
